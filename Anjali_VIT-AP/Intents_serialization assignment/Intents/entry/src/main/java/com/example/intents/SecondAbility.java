086db1cf7f12032da086a002833cdb7c692b685c
anjaliram7 <69080715+anjaliram7@users.noreply.github.com>
2021-10-06 12:05:29 +0530

    Added first and second assignment

---
+++ Anjali_VIT-AP/Intents_serialization assignment/Intents/entry/src/main/java/com/example/intents/SecondAbility.java
@@ -0,0 +1,45 @@
+package com.example.intents;
+
+
+import ohos.aafwk.ability.Ability;
+import ohos.aafwk.ability.AbilitySlice;
+import ohos.aafwk.content.Intent;
+import ohos.agp.components.Text;
+
+public class SecondAbility extends Ability {
+
+    Text userInfoName;
+    Text userInfoRoll;
+    Text userInfoUniversity;
+    Text userInfoDepartment;
+
+
+//    intent.setParam("user_info_name", user.getStudentName());
+//            intent.setParam("user_info_roll", user.getRollNo());
+//            intent.setParam("user_info_Department", user.getDepartmentName());
+//            intent.setParam("user_info_college", user.getCollegeName());
+//            intent.setParam("user_info_university", user.getUniversityName());
+
+    @Override
+    protected void onStart(Intent intent) {
+        super.onStart(intent);
+        super.setUIContent (ResourceTable.Layout_ability_next);
+        initViews (intent);
+
+    }
+
+    private void initViews (Intent intent) {
+        userInfoName = (Text) findComponentById(ResourceTable.Id_user_info_name);
+        userInfoRoll = (Text) findComponentById(ResourceTable.Id_user_info_roll);
+        userInfoUniversity = (Text) findComponentById(ResourceTable.Id_user_info_university);
+        userInfoDepartment = (Text) findComponentById(ResourceTable.Id_user_info_Department);
+
+        if (intent != null) {
+            userInfoName.setText(intent.getStringParam("user_info_name"));
+            userInfoRoll.setText(intent.getStringParam("user_info_roll"));
+            userInfoUniversity.setText(intent.getStringParam("user_info_university"));
+            userInfoDepartment.setText(intent.getStringParam("user_info_Department"));
+        }
+
+    }
+}
