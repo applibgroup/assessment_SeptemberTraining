53f3554cab057bc6968669dd7033eb2e52ba7f95
Mahesh Dasika <dasikamahesh1@gmail.com>
2021-12-30 17:15:34 +0530

    Assignment-I :Mahesh Dasika

---
+++ UI_Components_and_Layouts/Assignment-I Mahesh Dasika/Assignment_1/entry/src/main/java/com/example/assignment_1/MainAbility.java
@@ -0,0 +1,13 @@
+package com.example.assignment_1;
+
+import com.example.assignment_1.slice.MainAbilitySlice;
+import ohos.aafwk.ability.Ability;
+import ohos.aafwk.content.Intent;
+
+public class MainAbility extends Ability {
+    @Override
+    public void onStart(Intent intent) {
+        super.onStart(intent);
+        super.setMainRoute(MainAbilitySlice.class.getName());
+    }
+}
