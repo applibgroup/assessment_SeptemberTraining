88719dd3afd2d525294b94436dfb4c609ca51174
Mahesh Dasika <dasikamahesh1@gmail.com>
2022-02-08 13:21:00 +0530

    Assignment 3

---
+++ UI_Components_and_Layouts/Assignment3/entry/src/main/java/com/example/assignment3/MyApplication.java
@@ -0,0 +1,10 @@
+package com.example.assignment3;
+
+import ohos.aafwk.ability.AbilityPackage;
+
+public class MyApplication extends AbilityPackage {
+    @Override
+    public void onInitialize() {
+        super.onInitialize();
+    }
+}
